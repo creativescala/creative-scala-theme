4793dedf620956b14347fc48028933f0971ad417
Noel Welsh <noel@noelwelsh.com>
2024-09-04 16:11:41 +0100

    Complete new TOC generation code

--- src/main/resources/creativescala/js/toc.js
+++ src/main/resources/creativescala/js/toc.js
@@ -1,84 +1,239 @@
 // Reformats table of contents so it can be styled as we desire
 function tocPatch() {
   // All the TOCs (there should be two, for mobile and desktop.)
-  const nodes = document.querySelectorAll('ul.nav-list');
+  const nodes = document.querySelectorAll("ul.nav-list");
 
-  // Node => Array[Array[Element]]
+  // type Hunk = {
+  //   type: Type,
+  //   name: String,
+  //   href: String,
+  //   active: Boolean,
+  //   children: Array[Hunk]
+  // }
+  // type Type = "Part" | "Chapter" | "Section"
+
+  // A Hunk with no children is a Chapter
   //
-  // Collect the chapters into a data structure
-  function makeChapters(node) {
-    // Array[Array[Element]]
-    //
-    // Each Element is a TOC link. The first element is the chapter title
-    const chapters = [];
+  // A Hunk with one level of children is a Chapter and those children are Sections
+  //
+  // A Hunk with two levels of children is a Part, which children who are
+  // Chapters, and those children have Section children.
+  //
+  // The input is a flat list of Nodes. We use clases on the Nodes to determine
+  // how they are organized into Hunks.
+  //
 
-    Array.from(node.children).forEach(item => {
-      if (item.classList.contains('level1')) {
-        chapters.push([item]);
-      } else {
-        const chapter = chapters.pop();
-        chapter.push(item);
-        chapters.push(chapter);
-      }
-    })
-
-    return chapters;
+  // (String, String, Boolean, Array[Hunk]) => Hunk
+  function part(name, href, active, children = []) {
+    return {
+      type: "Part",
+      name: name,
+      href: href,
+      active: active,
+      children: children,
+    };
   }
 
-  // Element => Element
-  //
-  // Input should be of the form <li><a href="...">title</a></li>
-  function makeChapterTitle(element) {
-    const header = document.createElement("h5");
-    header.replaceChildren(...element.children);
+  // (String, String, Boolean, Array[Hunk]) => Hunk
+  function chapter(name, href, active, children = []) {
+    return {
+      type: "Chapter",
+      name: name,
+      href: href,
+      active: active,
+      children: children,
+    };
+  }
 
-    return header;
+  // (String, String, Boolean, Array[Hunk]) => Hunk
+  function section(name, href, active, children) {
+    return {
+      type: "Section",
+      name: name,
+      href: href,
+      active: active,
+      children: children,
+    };
+  }
+
+  // Element => String
+  function itemName(item) {
+    return item.children[0].textContent;
+  }
+
+  // Element => String
+  function itemHref(item) {
+    return item.children[0].getAttribute("href");
   }
 
   // Element => Boolean
-  function isActive(element) {
-    return element.classList.contains('active');
+  function itemIsActive(item) {
+    return item.classList.contains("active");
   }
 
-  // Array[Element] => Element
-  function makeChapterToc(chapter) {
-    const first = chapter.shift();
-    const title = makeChapterTitle(first);
+  // Array[Element] => Array[Hunk]
+  function itemsToHunks(items) {
+    let hunks = [];
 
-    if (first.classList.contains('nav-leaf')) {
-      const li = document.createElement('li');
-      li.className = first.className;
-      li.appendChild(title);
-      return li;
+    while (items.length > 0) {
+      let item = items.shift();
+      let hunk = itemToHunk(item, items);
+
+      hunks.push(hunk);
+    }
+
+    return hunks;
+  }
+
+  // (Element, Array[Element]) => Hunk
+  //
+  // Rest is mutated
+  function itemToHunk(item, rest) {
+    let hunk = {};
+
+    if (item.classList.contains("nav-leaf")) {
+      // It's a stand alone hunk with no children
+      hunk = chapter(itemName(item), itemHref(item), itemIsActive(item));
     } else {
-      const children = [];
-
-      const summary = document.createElement('summary');
-      summary.appendChild(title);
-      children.push(summary);
-
-      const list = document.createElement('ul');
-      list.replaceChildren(...chapter);
-      children.push(list);
-
-      const details = document.createElement('details');
-      if (isActive(first) || chapter.some(isActive)) {
-        details.setAttribute('open', 'true');
+      let [type, children] = itemChildren(2, rest);
+      switch (type) {
+        case "Part":
+          hunk = part(
+            itemName(item),
+            itemHref(item),
+            itemIsActive(item),
+            children,
+          );
+        case "Chapter":
+          hunk = chapter(
+            itemName(item),
+            itemHref(item),
+            itemIsActive(item),
+            children,
+          );
       }
-      details.replaceChildren(...children);
+    }
 
-      const li = document.createElement('li');
-      li.className = first.className;
-      li.appendChild(details);
-      return li;
+    return hunk;
+  }
+
+  function childType(type) {
+    switch (type) {
+      case "Part":
+        return "Chapter";
+      case "Chapter":
+        return "Section";
     }
   }
 
+  // (Int, Array[Element]) => [Type, Array[Hunk]]
+  //
+  // Rest is mutated
+  function itemChildren(level, rest) {
+    let children = [];
+    let type = "Chapter";
+
+    while (typeof (item = rest.shift()) !== "undefined") {
+      if (item.classList.contains("level1")) {
+        // This item is a top-level hunk so we've finished finding the children.
+        children.forEach((c) => (c.type = childType(type)));
+        rest.unshift(item);
+
+        return [type, children];
+      } else if (item.classList.contains("level2")) {
+        if (level > 2) {
+          // We've finished finding the children for the prior element
+          children.forEach((c) => (c.type = "Section"));
+          rest.unshift(item);
+
+          return [type, children];
+        } else {
+          let hunk = {
+            name: itemName(item),
+            href: itemHref(item),
+            isActive: itemIsActive(item),
+            children: []
+          };
+
+          children.push(hunk);
+          continue;
+        }
+      } else if (item.classList.contains("level3")) {
+        if (level < 3) {
+          type = "Part";
+          rest.unshift(item);
+
+          return itemChildren(3, rest);
+        } else {
+          let hunk = section(
+            itemName(item),
+            itemHref(item),
+            itemIsActive(item),
+          );
+
+          children.push(hunk);
+          continue;
+        }
+      } else {
+        throw new Error("Unhandled item", item);
+      }
+    }
+  }
+
+  // Array[Hunk] => Array[Element]
+  function hunksToToc(hunks) {
+    return hunks.map((hunk) => hunkToTocItem(hunk));
+  }
+
+  function hunkToTocItem(hunk) {
+    let item = document.createElement("li");
+
+    let link = document.createElement("a");
+    link.setAttribute("href", hunk.href);
+    link.appendChild(document.createTextNode(hunk.name));
+
+    if (hunk.children.length == 0) {
+      item.classList.add("nav-leaf");
+      item.appendChild(link);
+    } else {
+      item.classList.add("nav-node");
+
+      let details = document.createElement("details");
+      let summary = document.createElement("summary");
+      let h5 = document.createElement("h5");
+      h5.append(link);
+      summary.append(h5);
+      details.append(summary);
+
+      if (hunk.active) {
+        details.setAttribute("open", "true");
+      }
+
+      item.appendChild(details);
+
+      let children = hunk.children.map((hunk) => hunkToTocItem(hunk));
+      item.append(...children);
+    }
+
+    switch (hunk.type) {
+      case "Part":
+        item.classList.add("part");
+        break;
+      case "Chapter":
+        item.classList.add("chapter");
+        break;
+      case "Section":
+        item.classList.add("section");
+        break;
+    }
+
+    return item;
+  }
+
   function makeToc() {
-    nodes.forEach(node => {
-      const chapters = makeChapters(node);
-      // console.log("chapters", chapters);
-      const toc = chapters.map(chapter => makeChapterToc(chapter));
+    nodes.forEach((node) => {
+      const hunks = itemsToHunks(Array.from(node.children));
+      const toc = hunksToToc(hunks);
 
       node.replaceChildren(...toc);
     });
@@ -87,4 +242,4 @@ function tocPatch() {
   makeToc();
 }
 // Run it when the page loads
-addEventListener('load', () => tocPatch());
+addEventListener("load", () => tocPatch());
